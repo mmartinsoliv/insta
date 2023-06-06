db16b402afc4c78a90cd29a1a3025a1ce616da3f
matheus <mathews_2010@outlook.com>
2023-06-06 09:34:43 -0300

    feat(sign-in): create form

--- app/(cadaster)/sign-in/page.tsx
+++ app/(cadaster)/sign-in/page.tsx
@@ -1,21 +1,21 @@
 'use client'
 
-import { Button, Input } from '@/components'
+import { Button, Form, Input } from '@/components'
 import Link from 'next/link'
 
 const SignIn = () => {
 
   return (
-    <form role="form" className="flex flex-col items-center gap-5 mt-28 w-80 m-[auto]">
+    <Form>
       <h1 aria-label="form title" role="heading" className="text-center text-2xl font-mono">Sign in to InstaPets</h1>
-      <Input id='email' type='email' placeholder='Email' />
-      <Input id="password" type="password" placeholder="Password"/>
+      <Input id='email' name="email" type='email' placeholder='Email' />
+      <Input id="password" name="password" type="password" placeholder="Password"/>
       <Link href="/forgot-password" className='ml-[auto] text-[#5F5F5F]'>Forgot your password?</Link>
       <Button>Log in</Button>
       <div>
         <div className='w-80'>-</div>
       </div>
-    </form>
+    </Form>
   )
 }
 
