6244fafd78f02ba69b4f642eae85df23e4fb7998
matheus <mathews_2010@outlook.com>
2023-06-06 09:35:46 -0300

    refactor(components): add new props to input component

--- components/input/input.tsx
+++ components/input/input.tsx
@@ -1,9 +1,29 @@
 
 type InputProps = {
-  id: string
+  name: string
   type: string
+  value: string
+  onChange: any
+  onBlur?: () => void
   placeholder: string
 }
 
-export const Input = ({ id, type, placeholder }: InputProps) => <input id={id} type={type} className="bg-[#2B2B2B] py-4 px-3 rounded-lg w-80" placeholder={placeholder} />
+export const Input = ({
+  type,
+  name,
+  value,
+  onChange,
+  onBlur,
+  placeholder }: InputProps) => (
+  <input
+    id={name}
+    type={type}
+    name={name}
+    value={value}
+    onChange={onChange}
+    onBlur={onBlur}
+    className="bg-[#2B2B2B] py-4 px-3 rounded-lg w-80"
+    placeholder={placeholder}
+  />
+)
 
