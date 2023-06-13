0d1138b2b01a6655671d1c9dabbb9309f02a4b48
matheus martins <mathews_2010@outlook.com>
2023-06-13 08:59:48 -0300

    refactor(input): change the api

--- components/input/input.tsx
+++ components/input/input.tsx
@@ -1,29 +1,10 @@
+import { InputHTMLAttributes } from "react"
 
 type InputProps = {
-  name: string
   type: string
-  value: string
-  onChange: any
-  onBlur?: () => void
   placeholder: string
-}
+} & InputHTMLAttributes<HTMLInputElement>
 
-export const Input = ({
-  type,
-  name,
-  value,
-  onChange,
-  onBlur,
-  placeholder }: InputProps) => (
-  <input
-    id={name}
-    type={type}
-    name={name}
-    value={value}
-    onChange={onChange}
-    onBlur={onBlur}
-    className="bg-[#2B2B2B] py-4 px-3 rounded-lg w-80"
-    placeholder={placeholder}
-  />
+export const Input = ({ type, placeholder, ...props}: InputProps) => (
+  <input type={type} placeholder={placeholder} {...props} className="bg-[#2B2B2B] py-4 px-3 rounded-lg w-80" />
 )
-
